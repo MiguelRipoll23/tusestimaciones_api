f911c3a91e0c689373d6bb4d41fee95932a5a415
Miguel Ripoll <3296866+MiguelRipoll23@users.noreply.github.com>
2023-04-23 15:57:43 +0200

    Update deps (#35)

--- deps.ts
+++ deps.ts
@@ -1,7 +1,7 @@
 export type {
   document,
   node,
-} from "https://deno.land/x/xml@2.1.0/utils/types.ts";
+} from "https://deno.land/x/xml@2.1.1/utils/types.ts";
 
-export { serve } from "https://deno.land/std@0.182.0/http/server.ts";
-export { parse } from "https://deno.land/x/xml@2.1.0/mod.ts";
+export { serve } from "https://deno.land/std@0.184.0/http/server.ts";
+export { parse } from "https://deno.land/x/xml@2.1.1/mod.ts";
